b6a55848cd20b64fe09a586c6cd6ef07d632c890
LuckyIndraEfendi <kimilbonchu@gmail.com>
2024-05-09 07:47:38 +0700

    delete helmet

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import helmet from "helmet";
 import cors from "cors";
 import bodyParser from "body-parser";
 import "dotenv/config";
@@ -9,7 +8,6 @@ import manhwa from "./routes/manhwa.js";
 import search from "./routes/search.js";
 const PORT = process.env.PORT || 5000;
 const app = express();
-app.use(helmet());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
