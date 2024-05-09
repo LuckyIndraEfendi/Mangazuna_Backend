1a48f3858f5efc09b0333bead7283b945755b149
LuckyIndraEfendi <kimilbonchu@gmail.com>
2024-05-09 07:50:15 +0700

    update useragent

--- helpers/userAgent.js
+++ helpers/userAgent.js
@@ -1,6 +1,6 @@
 const userAgent = {
   "User-Agent":
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows; U; Windows NT 10.4; Win64; x64; en-US) Gecko/20100101 Firefox/62.9",
 };
 
 export default userAgent;
